ae79c58344f6d86f8fc7eca9e1d87679a82f5ea2
Dharmendra7777 <mahida.dm@gmail.com>
2018-08-19 09:08:55 +0530

    code snippet project first commit

---
+++ Django_code_snippet/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dharmendra/source/Django/code_snippet/Django_code_snippet/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
